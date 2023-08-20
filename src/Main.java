a4aac49f26bba743a1583066ec89accfc09cd28d
redmalice <134430632+redmalice@users.noreply.github.com>
2023-08-20 19:36:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int amount = 13676;
+        int sale = 20;
+        int bonus;
+        bonus = amount / sale;
+
+        System.out.println((bonus) + " Начисленное кол-во бонусов ");
+
+    }
+}
